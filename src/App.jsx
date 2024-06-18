2d2d65ef4d1297b16fce6b86a7668320f341b6c7
Ankush Goel <goelankush1312@gmail.com>
2024-06-18 23:15:34 +0530

    Added dispatch functions to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import Counter from './components/Counter'
+import { useDispatch } from 'react-redux'
 
 function App() {
   const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -16,11 +18,11 @@ function App() {
 
       <h1>Using Redux</h1>
       <div className="card">
-        <button>
+        <button onClick={(e) => dispatch({type: "INCREMENT"})}>
           Increment
         </button>
         <Counter />
-        <button>
+        <button onClick={(e) => dispatch({type: "DECREMENT"})}>
           Decrement
         </button>
       </div>
